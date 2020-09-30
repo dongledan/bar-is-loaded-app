39def797d0ec47b1bced62c2ea6aefd72bb4029c
Danny Li <dongledan@yahoo.com>
2020-09-30 18:37:01 -0400

    content

--- src/components/Content.js
+++ src/components/Content.js
@@ -11,18 +11,25 @@ import image6 from '../images/img-6.png';
 export default function Content() {
   return (
     <div className="content-container">
-       <div className="content-title">The Everyday Gym Calculator</div>
+       <div className="content-title">Your Everyday Gym Calculator</div>
         <div className="content-title-content">Quick maths is hard, especially when the pre-workout kicks in. <strong>Bar Is Loaded</strong> takes the math out of the gym and displays exactly what you need for your next lift. It doesn't matter if it's in <strong>kilograms</strong> or <strong>pounds</strong>, we got you covered. </div>
       <div className="content-grid">
+      <h2 className="content-header multi">
+        Customize. Calculate. Lift.
+        <div className="content-line" />
+      </h2>
+        <div className="content-subheader">No reds, all blues? Don't be blue! Change your <strong>inventory</strong> seamlessly. It's never been easier with <strong>Bar Is Loaded</strong>.</div>
         <div className="content-image">
           <img src={image0} className="image one" />
           <img src={image2} className="image two" />
         </div>
-        <h2>Customize. Calculate. Lift.</h2>
-        <div className="content-subheader">No reds, all blues? Don't be blue! Change your <strong>inventory</strong> seamlessly. It's never been easier with <strong>Bar Is Loaded</strong>.</div>
       </div>
       <div className="content-grid column-length">
-        <h2>Suggestions.</h2>
+        <h2 className="content-header-2">
+          Suggestions.
+          <div className="content-line" />
+
+        </h2>
         <div className="content-subheader-2">A lift has stalled and you've tried everything. We've all been there. Here are some <strong>exercises</strong> to help break through.</div>
         <div className="content-image">
           <img src={image3a} className="image one" />
@@ -30,14 +37,16 @@ export default function Content() {
         </div>
       </div>
       <div className="content-grid">
-      <div className="content-image">
+        <h2 className="content-header">
+          All In One.
+          <div className="content-line" />
+        </h2>
+        <div className="content-subheader">Upcoming <strong>powerlifting meet</strong>? Got <strong>Wilks</strong>? <strong>RPE</strong>/<strong>RIR</strong> training? <strong>Bar Is Loaded</strong> has it all. It's more than just calculations.</div>
+        <div className="content-image">
           <img src={image4} className="image one" />
           <img src={image5} className="image three" />
           <img src={image6} className="image four" />
-
         </div>
-        <h2>All In One.</h2>
-        <div className="content-subheader">Upcoming <strong>powerlifting meet</strong>? Got <strong>Wilks</strong>? <strong>RPE</strong>/<strong>RIR</strong> training? <strong>Bar Is Loaded</strong> has it all. It's more than just calculations.</div>
       </div>
      
     </div>
